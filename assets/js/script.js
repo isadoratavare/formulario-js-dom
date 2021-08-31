0cfe817a9bf4d1efd286e553160f1a4e9a7b64ee
isadoratavare <isadoratavare85@gmail.com>
2021-08-31 19:24:23 -0300

    validando campo senha

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,43 @@ function emptyInput(){
     let senha = document.getElementById("senha").value
     if(email == ''){
         document.getElementById("email").className="empty"
-        document.getElementById("text-email").className = "msg2"
+        document.getElementById("text-email").innerHTML = "* O campo email é obrigatório"
     }else{
         document.getElementById("email").className="email"
-        document.getElementById("text-email").className = "msg"
+        document.getElementById("text-email").innerHTML = ""
     }
     if (senha == ''){
         document.getElementById("senha").className="empty"
-        document.getElementById("text-password").className = "msg2"
+        document.getElementById("text-password").innerHTML = "* O campo senha é obrigatório"
     }
     else{
         document.getElementById("senha").className="email"
-        document.getElementById("text-password").className = "msg"
-
-        
+        document.getElementById("text-password").innerHTML = ""   
     }
 }
 function emailCorrect(){
+    let email = document.getElementById("email").value
+    if (email != ""){
+        for (i of email){
+            if (i == "@"){
+                document.getElementById("email").className="email"
+                document.getElementById("text-email").innerHTML = ""
+            } else{
+                document.getElementById("email").className="empty"
+                document.getElementById("text-email").innerHTML = "* Email incorreto"
+            }
+        }
+    }
+}
+function passwordCorrect(){
+    let senha = document.getElementById("senha").value
+    if(senha != ""){
+        if(senha.length < 8){
+        document.getElementById("senha").className="empty"
+        document.getElementById("text-password").innerHTML = "* A senha deve possuir mais de 8 caracteres"}
+    } else{
+        document.getElementById("senha").className="email"
+        document.getElementById("text-password").innerHTML = ""
 
+    }
 }
