5ac708e16f1f2372bb48d553af66e259f190fc75
isadoratavare <isadoratavare85@gmail.com>
2021-09-01 10:49:14 -0300

    bug input senha

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ function emptyInput(){
     let email = document.getElementById("email").value
     let senha = document.getElementById("senha").value
     if(email == ''){
+        console.log("email vazio")
         document.getElementById("email").className="empty"
         document.getElementById("text-email").innerHTML = "* O campo email é obrigatório"
     }else{
@@ -9,6 +10,7 @@ function emptyInput(){
         document.getElementById("text-email").innerHTML = ""
     }
     if (senha == ''){
+        console.log("senha vazio")
         document.getElementById("senha").className="empty"
         document.getElementById("text-password").innerHTML = "* O campo senha é obrigatório"
     }
@@ -35,9 +37,10 @@ function passwordCorrect(){
         if(senha.length < 8){
         document.getElementById("senha").className="empty"
         document.getElementById("text-password").innerHTML = "* A senha deve possuir mais de 8 caracteres"}
-    } else{
-        document.getElementById("senha").className="email"
-        document.getElementById("text-password").innerHTML = ""
-
-    }
+        else{
+            document.getElementById("senha").className="email"
+            document.getElementById("text-password").innerHTML = ""
+    
+        }
+    } 
 }
